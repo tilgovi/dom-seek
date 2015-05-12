b0ea258bc02b5ae6b591078386e4dcb31dde18da
Randall Leeds <randall@bleeds.info>
2015-05-12 11:32:55 -0700

    Update dist

--- seek.js
+++ seek.js
@@ -10,66 +10,124 @@ exports.install = install;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-/** @module text-walker */
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
 /* Internal constants */
 
-var E_NODE = 'Argument 1 of TextIterator.seek is neither Element nor Text.';
-var E_TYPE = 'Argument 1 of TextIterator.seek is neither Numer nor Node.';
-var SEEKABLE_NODES = [Node.ELEMENT_NODE, Node.TEXT_NODE, Node.DOCUMENT_NODE];
+var E_ROOT = 'Argument 1 of createTextIterator is not an Element.';
+var E_SEEK = 'Argument 1 of TextIterator.seek is neither a number nor Text.';
+
+var FRAME_RATE = 60;
+var TICK_LENGTH = 1000 / FRAME_RATE;
 
 /* Public interface */
 
-var TextIterator = function TextIterator() {
-  _classCallCheck(this, TextIterator);
-};
+var TextIterator = (function (_NodeIterator) {
+  function TextIterator() {
+    _classCallCheck(this, TextIterator);
+
+    if (_NodeIterator != null) {
+      _NodeIterator.apply(this, arguments);
+    }
+  }
+
+  _inherits(TextIterator, _NodeIterator);
+
+  return TextIterator;
+})(NodeIterator);
 
 exports.TextIterator = TextIterator;
 ;
 
 function createTextIterator(root, filter) {
   var document = global.document;
+
+  if (root.nodeType !== Node.ELEMENT_NODE) {
+    throw new UnsupportedError(E_ROOT);
+  }
+
   var iter = document.createNodeIterator(root, NodeFilter.SHOW_TEXT, filter);
 
+  // IE compatibility
+  if (typeof iter.referenceNode === 'undefined') {
+    iter.referenceNode = root;
+    iter.pointerBeforeReferenceNode = true;
+  }
+
+  // NodeIterator doesn't have a proper constructor so we don't either :(
+  // Also, this keeps the NodeIterator private.
   return Object.create(TextIterator.prototype, {
     root: {
-      value: root
+      get: function get() {
+        return iter.root;
+      }
     },
 
     whatToShow: {
-      value: NodeFilter.SHOW_TEXT
+      get: function get() {
+        return iter.whatToShow;
+      }
     },
 
     filter: {
-      value: filter
+      get: function get() {
+        return iter.filter;
+      }
     },
 
     referenceNode: {
       get: function get() {
-        if (iter.referenceNode) {
-          return iter.referenceNode;
-        } else {
-          // IE portability
-          iter.nextNode();
-          return iter.previousNode();
+        return iter.referenceNode;
+      }
+    },
+
+    pointerBeforeReferenceNode: {
+      get: function get() {
+        return iter.pointerBeforeReferenceNode;
+      }
+    },
+
+    nextNode: {
+      value: function value() {
+        var result = iter.nextNode();
+
+        // IE compatibility
+        if (typeof iter.referenceNode === 'undefined') {
+          iter.referenceNode = result;
+          iter.pointerBeforeReferenceNode = false;
         }
+
+        return result;
+      }
+    },
+
+    previousNode: {
+      value: function value() {
+        var result = iter.previousNode();
+
+        // IE compatibility
+        if (typeof iter.referenceNode === 'undefined') {
+          iter.referenceNode = result;
+          iter.pointerBeforeReferenceNode = true;
+        }
+
+        return result;
       }
     },
 
     seek: {
       value: function value(where) {
-        var self = this;
+        // Pre-condition: pointerBeforeReferenceNode
+        if (!this.pointerBeforeReferenceNode) {
+          this.previousNode();
+        }
 
-        if (!isNaN(parseInt(where)) && isFinite(where)) {
-          return seek_offset(iter, parseInt(where));
-        } else if (where.nodeType) {
-          if (SEEKABLE_NODES.indexOf(where.nodeType) !== -1) {
-            return seek_node(iter, where);
-          } else {
-            return Promise.reject(new TypeError(E_NODE));
-          }
+        if (isNumber(where)) {
+          return seek_offset(this, parseInt(where));
+        } else if (isText(where)) {
+          return seek_node(this, where);
         } else {
-          return Promise.reject(new TypeError(E_TYPE));
+          return Promise.reject(new UnsupportedError(E_SEEK));
         }
       }
     }
@@ -90,6 +148,18 @@ function install() {
 
 /* Private implementation */
 
+function isNumber(n) {
+  return !isNaN(parseInt(n)) && isFinite(n);
+}
+
+function isText(node) {
+  if (typeof node.nodeType === 'number') {
+    return node.nodeType === Node.TEXT_NODE;
+  } else {
+    return false;
+  }
+}
+
 function before(referenceNode, node) {
   return referenceNode.compareDocumentPosition(node) & (Node.DOCUMENT_POSITION_FOLLOWING | Node.DOCUMENT_CONTAINED_BY);
 }
@@ -99,34 +169,74 @@ function after(referenceNode, node) {
 }
 
 function seek_node(iter, node) {
-  function forward(count) {
-    var curNode = iter.nextNode();
-    if (curNode === null) {
-      return Promise.resolve(count);
-    } else {
+  var yieldAt = Date.now() + TICK_LENGTH;
+
+  function forward(_x) {
+    var _again = true;
+
+    _function: while (_again) {
+      curNode = undefined;
+      _again = false;
+      var count = _x;
+
+      var curNode = iter.nextNode();
+
+      if (curNode === null) {
+        return Promise.resolve(count);
+      }
+
       count += curNode.textContent.length;
+
       if (after(curNode, node)) {
         return Promise.resolve(count);
+      }
+
+      if (Date.now() < yieldAt) {
+        _x = count;
+        _again = true;
+        continue _function;
       } else {
-        return new Promise(function (r) {
-          return setTimeout(r, 0, count);
-        }).then(forward);
+        return new Promise(function (resolve) {
+          requestAnimationFrame(function () {
+            yieldAt = Date.now() + TICK_LENGTH;
+            resolve(forward(count));
+          });
+        });
       }
     }
   }
 
-  function backward(count) {
-    var curNode = iter.previousNode();
-    if (curNode === null) {
-      return Promise.resolve(count);
-    } else {
+  function backward(_x2) {
+    var _again2 = true;
+
+    _function2: while (_again2) {
+      curNode = undefined;
+      _again2 = false;
+      var count = _x2;
+
+      var curNode = iter.previousNode();
+
+      if (curNode === null) {
+        return Promise.resolve(count);
+      }
+
       count -= curNode.textContent.length;
+
       if (curNode === node || before(curNode, node)) {
         return Promise.resolve(count);
+      }
+
+      if (Date.now() < yieldAt) {
+        _x2 = count;
+        _again2 = true;
+        continue _function2;
       } else {
-        return new Promise(function (r) {
-          return setTimeout(r, 0, count);
-        }).then(backward);
+        return new Promise(function (resolve) {
+          requestAnimationFrame(function () {
+            yieldAt = Date.now() + TICK_LENGTH;
+            resolve(backward(count));
+          });
+        });
       }
     }
   }
@@ -135,34 +245,74 @@ function seek_node(iter, node) {
 }
 
 function seek_offset(iter, offset) {
-  function forward(count) {
-    var curNode = iter.nextNode();
-    if (curNode === null) {
-      return Promise.resolve(count);
-    } else {
+  var yieldAt = Date.now() + TICK_LENGTH;
+
+  function forward(_x3) {
+    var _again3 = true;
+
+    _function3: while (_again3) {
+      curNode = undefined;
+      _again3 = false;
+      var count = _x3;
+
+      var curNode = iter.nextNode();
+
+      if (curNode === null) {
+        return Promise.resolve(count);
+      }
+
       count += curNode.textContent.length;
+
       if (count > offset) {
         return Promise.resolve(count);
+      }
+
+      if (Date.now() < yieldAt) {
+        _x3 = count;
+        _again3 = true;
+        continue _function3;
       } else {
-        return new Promise(function (r) {
-          return setTimeout(r, 0, count);
-        }).then(forward);
+        return new Promise(function (resolve) {
+          requestAnimationFrame(function () {
+            yieldAt = Date.now() + TICK_LENGTH;
+            resolve(forward(count));
+          });
+        });
       }
     }
   }
 
-  function backward(count) {
-    var curNode = iter.previousNode();
-    if (curNode === null) {
-      return Promise.resolve(count);
-    } else {
+  function backward(_x4) {
+    var _again4 = true;
+
+    _function4: while (_again4) {
+      curNode = undefined;
+      _again4 = false;
+      var count = _x4;
+
+      var curNode = iter.previousNode();
+
+      if (curNode === null) {
+        return Promise.resolve(count);
+      }
+
       count -= curNode.textContent.length;
+
       if (count <= offset) {
         return Promise.resolve(count);
+      }
+
+      if (Date.now() < yieldAt) {
+        _x4 = count;
+        _again4 = true;
+        continue _function4;
       } else {
-        return new Promise(function (r) {
-          return setTimeout(r, 0, count);
-        }).then(backward);
+        return new Promise(function (resolve) {
+          requestAnimationFrame(function () {
+            yieldAt = Date.now() + TICK_LENGTH;
+            resolve(backward(count));
+          });
+        });
       }
     }
   }
