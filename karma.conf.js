ab8d7bf49a3eaa2723b0c1ba30e6e6c581e73f9e
Randall Leeds <randall@bleeds.info>
2015-05-20 17:41:14 -0700

    Sauce

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,16 @@
+var fs = require('fs');
+
+// Use ENV vars on Travis and sauce.json locally to get credentials
+if (!process.env.SAUCE_USERNAME) {
+  if (!fs.existsSync('sauce.json')) {
+    console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+    process.exit(1);
+  } else {
+    process.env.SAUCE_USERNAME = require('./sauce').username;
+    process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+  }
+}
+
 module.exports = function(config) {
   config.set({
     browsers: ['Firefox'],
@@ -26,6 +39,21 @@ module.exports = function(config) {
         {type: 'lcovonly'},
         {type: 'text'}
       ]
-    }
+    },
+    customLaunchers: {
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome'
+      },
+      'SL_IE': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        version: '11'
+      }
+    },
+    sauceLabs: {
+      testName: 'DOM Seek'
+    },
+    singleRun: true
   });
 };
